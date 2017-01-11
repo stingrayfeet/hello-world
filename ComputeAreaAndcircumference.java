7ca5fad11838b5cfe04706b021d071b3fe975869
stingrayfeet <richardwblake@ufl.edu>
2017-01-11 18:14:39 -0500

    Add files via upload

---
+++ ComputeAreaAndcircumference.java
@@ -0,0 +1,22 @@
+
+public class ComputeAreaAndcircumference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//declaring and assigning variables
+		final double PI = Math.PI;
+		double radius = 20.0;
+		double area ;
+		double circumference;
+		
+		//compute area and circumference
+		area = radius * radius * PI;
+		circumference = 2 * radius;
+		
+		//print out calculation
+		System.out.println("The area of circle with a radius " + radius + " is " + area);
+		System.out.println("The circumference of the circle with a radius " + radius +" is " + circumference);
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// Java file created in class on 1/10/16
+public class HelloWorld {
+  public static void main(String args[]) {
+    System.out.println("Hello World!");
+    System.out.println("Name: Richard (Wills) Blake");
+    System.out.println("Major: Linguistics");
+    System.out.println("Been at UF for 2 years");
+    System.out.println("I like Thai food");
+  }
+}
